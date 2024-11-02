45315b2ad347272054563c0262b5276caf2b2879
Projectwork4thyear <prakritisewa04@gmail.com>
2024-11-02 21:22:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ def get_user_profile(user_id):
         return jsonify({'error': 'User not found'}), 404
         
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port = 8080)
